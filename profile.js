296e66b3398d4fd8d558adfb6a4df5ec6435889d
Tranvietducc <114197795+Tranvietducc@users.noreply.github.com>
2024-01-06 08:56:29 +0700

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,20 @@
+{
+    "name": "Zoebaka",
+    "version": "3.5.0",
+    "description": "Zoe",
+    "main": "mirai.js",
+    "scripts": {
+        "start": "node index",
+        "login": "node login",
+        "test": "node --trace-warnings --use_strict --async-stack-traces mirai"
+    },
+    "repository": {
+        "type": "git",
+        "url": "git+https://github.com/D-Jukie/miraiv2.git"
+    },
+    "author": "Zoebakaa",
+    "license": "GPL-3.0",
+    "dependencies": {
+        "@miraipr0ject/assets": ""
+    }
+}
